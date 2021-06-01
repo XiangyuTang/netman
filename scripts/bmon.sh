e48f933495f14c25f8c2f367f0bf7aa05e96caa3
XiangyuTang <tangxy_01@163.com>
2021-06-01 23:29:58 +0800

    Bump version 1.0.54

--- scripts/bmon.sh
+++ scripts/bmon.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ $(whoami) != "root" ]; then
+    echo -e "\e[1;31m 请以root权限运行 \e[0m"
+    exit
+fi
+
 is_bmon_installed=($(dpkg -s bmon|grep Status))
 if [ ${is_bmon_installed[2]} != "ok" ]
 then
--- scripts/vnstat.sh
+++ scripts/vnstat.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # https://vircloud.net/linux/netmo-vnstat.html
+if [ $(whoami) != "root" ]; then
+    echo -e "\e[1;31m 请以root权限运行 \e[0m"
+    exit
+fi
+
 is_installed=($(dpkg -s vnstat|grep Status))
 if [ ${is_installed[2]} != "ok" ]
 then
